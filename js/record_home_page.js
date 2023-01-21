854a351c517c3970c68f0290543f57f06741c750
adam nunez <adam.a.nunez@gmail.com>
2023-01-21 15:16:45 -0600

    Cleanup record home page js

--- js/record_home_page.js
+++ js/record_home_page.js
@@ -1,21 +1,30 @@
-CallLog.link = "";
-CallLog.fn.buildCallLogBtn = () => {
 
-    let callIcon = `<a class="CallLogLink"><i class="fa fa-phone"></i></a>`;
-    let systemTable = $(`.sysManTable [data-mlm-name=${CallLog.static.instrument}]`).closest('td');
-    let redcapTable = $(`#event_grid_table [data-mlm-name=${CallLog.static.instrument}]`).closest('tr');
+$(document).ready(() => {
 
+    let callLogLink = "";
+    const callIcon = `<a class="CallLogLink"><i class="fa fa-phone"></i></a>`;
+    const systemTable = $(`.sysManTable [data-mlm-name=${CallLog.static.instrument}]`).closest('td');
+    const redcapTable = $(`#event_grid_table [data-mlm-name=${CallLog.static.instrument}]`).closest('tr');
+
+    // Prep the styled button
+    $('head').append(`<style>.CallLogLink { cursor: pointer; }</style>`)
+    $("body").on("click", ".CallLogLink", () => { location.href = callLogLink; });
+
+    // Hide the Call Log repeating instrument table
+    $(`#repeat_instrument_table-${CallLog.static.instrumentEvent}-${CallLog.static.instrument}`).parent().remove();
+
+    // Replace Call Log icons with the phone
     systemTable.add(redcapTable).find('button, a').each((_, el) => {
         if ($(el).hasClass("invis")) {
             return;
         }
         // First instance, deprioritized
-        if ($(el).is('a') && !CallLog.link) {
-            CallLog.link = $(el).prop('href');
+        if ($(el).is('a') && !callLogLink) {
+            callLogLink = $(el).prop('href');
         }
         // Any other instance
         if ($(el).is('button')) {
-            CallLog.link = $(el).attr('onclick').split(`='`)[1].replace(`';`, '');
+            callLogLink = $(el).attr('onclick').split(`='`)[1].replace(`';`, '');
         }
         // Insert the button
         if ($(".CallLogLink").length < 1) {
@@ -23,18 +32,6 @@ CallLog.fn.buildCallLogBtn = () => {
         }
         $(el).hide();
     });
-}
 
-$(document).ready(() => {
-
-    // Prep the styled button
-    $('head').append(`<style>.CallLogLink { cursor: pointer; }</style>`)
-    $("body").on("click", ".CallLogLink", () => { location.href = CallLog.link; });
-
-    // Hide the Call Log repeating instrument table
-    $(`#repeat_instrument_table-${CallLog.static.instrumentEvent}-${CallLog.static.instrument}`).parent().remove();
-
-    // Replace Call Log icons with the phone
-    CallLog.fn.buildCallLogBtn();
 });
 
